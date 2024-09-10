cc7e1b472095a5149a320b0498d89e4ce3579ad7
Szymon <szymon.skowron@ardium.pl>
2024-09-11 00:34:55 +0200

    Initial commit

---
+++ mysql_db_queries.py
@@ -0,0 +1,102 @@
+from mysql_db_config import create_connection_pool, close_connection
+from logger import logger_mysql
+import mysql.connector
+from mysql.connector.errors import Error as MySQL_Error
+from mysql.connector.errors import PoolError
+import time
+
+
+# Create connection pool on app init
+pool = create_connection_pool()
+
+
+def insert_customer_mysql(first_name, last_name, email, employee_id):
+    logger_mysql.info("⚙️ Inserting a new customer...")
+    connection, cursor = None, None
+    try:
+        connection = pool.get_connection()
+        cursor = connection.cursor()
+        time.sleep(3)
+
+        query = "INSERT INTO customers (first_name, last_name, email, served_by_employee_id) VALUES (%s, %s, %s, %s)"
+        data = (first_name, last_name, email, employee_id)
+
+        cursor.execute(query, data)
+        connection.commit()
+
+        # Print lastrowid
+        last_row_id = cursor.lastrowid
+        logger_mysql.info(f'⚙️ Last row id from the cursor object: {last_row_id}')
+
+        # Check if the insert was successful
+        if cursor.rowcount > 0:
+            logger_mysql.info("✅ New record inserted successfully.")
+            message = "✅ New record inserted successfully."
+            return message, 201
+        else:
+            raise RuntimeError("❌ Failed to insert a new record.")
+
+    except PoolError as e:
+        logger_mysql.error(
+            "❌ An error occurred during performing an insert query. Couldn't obtain a connection from the pool.")
+        logger_mysql.error(f"Error message: {e}")
+        message = "❌ Failed to insert a new record."
+        return message, 500
+
+    except Exception as e:
+        logger_mysql.error("❌ An error occurred during performing an insert query.")
+        logger_mysql.error(f"Error message: {e}")
+        message = "❌ Failed to insert a new record."
+        return message, 500
+
+    finally:
+        close_connection(connection, cursor)
+
+
+def get_products_mysql(category):
+    logger_mysql.info("⚙️ Getting products for requested category...")
+    connection, cursor = None, None
+    try:
+        connection = pool.get_connection()
+        cursor = connection.cursor()
+        time.sleep(3)
+
+        query = "SELECT * FROM products WHERE category = %s"
+        data = (category,)
+        cursor.execute(query, data)
+
+        rows = cursor.fetchall()
+        if rows:
+            message = f"We have the following products which belong to {category} category:"
+            for row in rows:
+                id, product_name, category, price, stock_quantity = row
+                message += f" 💠{product_name}: ${price}"
+        else:
+            message = f"Unfortunately we don't have any products which belong to {category} category in our catalog."
+
+        # # TEST TEST TEST
+        # insert_customer_mysql(
+        #     first_name="Alice",
+        #     last_name="Johnson",
+        #     email="alice.johnson@example.com",
+        #     employee_id=1
+        # )
+
+        logger_mysql.info("✅ SELECT query executed successfully.")
+        return message, 200
+
+    except PoolError as e:
+        logger_mysql.error(
+            "❌ An error occurred during querying the database. Couldn't obtain a connection from the pool.")
+        logger_mysql.error(f"Error message: {e}")
+        message = "❌ Failed to retrieve the data."
+        return message, 500
+
+    except Exception as e:
+        logger_mysql.error("❌ An error occurred during querying the database.")
+        logger_mysql.error(f"Error message: {e}")
+        message = "❌ Failed to retrieve the data."
+        return message, 500
+
+    finally:
+        close_connection(connection, cursor)
